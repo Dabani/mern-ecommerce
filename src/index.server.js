6fad539f677cb3f5dac68256338eac4360eb19ef
DabaniCode <dabani.rm@gmail.com>
2021-05-28 09:56:21 +0200

    Initial Commit

---
+++ src/index.server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const mongoose = require('mongoose');
+
+// Routes
+const userRoutes = require('./routes/user');
+
+// Environment variable or you can say constants
+env.config();
+
+// Connect to MongoDB database
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@onlinedatingapp.m6dxp.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+{ 
+  useNewUrlParser: true, useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(() => {
+  console.log('Database connected')
+})
+.catch((err) => {
+  console.log(err);
+});
+
+app.use(express.json());
+app.use('/api', userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
